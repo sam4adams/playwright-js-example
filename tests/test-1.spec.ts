b583c50c1471c9a2e63f4f3ecba1249d8151997c
sam4adams <sam4adams@gmail.com>
2024-05-04 15:14:15 -0700

    New project to push to github

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.getByLabel('Google apps').click();
+  await page.frameLocator('iframe[name="app"]').getByRole('link', { name: 'Gmail' }).click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByLabel('Email or phone').click();
+  await page.getByLabel('Email or phone').fill('sadams@gmail.com');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByText('Couldn’t find your Google').click();
+  await expect(page.getByText('Couldn’t find your Google')).toBeVisible();
+});
